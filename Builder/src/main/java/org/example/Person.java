58425eec735502e8f26935c981a3b49e80738af9
Vlad1slave <127949635+Vlad1slave@users.noreply.github.com>
2025-01-31 16:35:09 +0700

    Add java project task1.1

---
+++ Builder/src/main/java/org/example/Person.java
@@ -0,0 +1,80 @@
+package org.example;
+
+import java.util.OptionalInt;
+
+public class Person {
+    private final String name;
+    private final String surname;
+    private Integer age;
+    private String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this(name, surname);
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age != null;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return age != null ? OptionalInt.of(age) : OptionalInt.empty();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) {
+            age++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + (hasAge() ? age : "unknown") +
+                ", address='" + (hasAddress() ? address : "unknown") + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + surname.hashCode();
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(this.surname)
+                .setAge(0)
+                .setAddress(this.address);
+    }
+}
